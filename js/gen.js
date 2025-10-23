369d50433d2c647580428a9e5938af50582ddee4
zarf0 <131638329+zarf0@users.noreply.github.com>
2025-10-23 23:25:31 +0530

    Update gen.js

--- js/gen.js
+++ js/gen.js
@@ -1,18 +1,4 @@
-<script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        charcoal: '#2C3E50',
-                        slate: '#4A6572',
-                        sand: '#E5DCC3',
-                        linen: '#F9F5EB',
-                        sage: '#A3B18A'
-                    }
-                }
-            }
-        }
-    </script>
+
 
 // loginAnimation.js
 
@@ -663,3 +649,4 @@ function displaySuggestions(suggestions) {
 
 
  
+
